44800dcc076e5491e71b74ce18c188a86c094928
Isaac <uniqucode@gmail.com>
2024-06-28 20:34:08 +0100

    Fix a bug

--- components/menuBar/MenuBar.tsx
+++ components/menuBar/MenuBar.tsx
@@ -12,7 +12,7 @@ function MenuBar() {
         <AlignLeft className="w-6 h-6" />
       </span>
       {open && (
-        <div className="absolute bg-[#F9F9F9] left-0 top-20 w-full h-[calc(100vh-80px)] flex flex-col items-center justify-center gap-4 text-base z-20">
+        <div className="absolute bg-[#F9F9F9] left-0 top-20 sm:top-16 w-full h-[calc(100vh-80px)] sm:h-[30rem] flex flex-col items-center justify-center gap-4 text-base z-20">
           <Link href="">About</Link>
           <Link href="">SIP</Link>
           <Link href="">Studio</Link>
@@ -20,16 +20,10 @@ function MenuBar() {
           <Link href="">Platforms</Link>
           <Link href="">Initiatives</Link>
           <Link href="">More</Link>
-          <button
-            className="text-[10px] text-white py-2 px-3 font-600 leading-[19.2px] bg-[#20888F] rounded-3xl
-]"
-          >
+          <button className="text-[10px] text-white py-2 px-3 font-600 leading-[19.2px] bg-[#20888F] rounded-3xl">
             SINC With Us
           </button>
-          <button
-            className="text-[10px] text-white py-2 px-3 font-600 leading-[19.2px] bg-[#303030] rounded-3xl
-]"
-          >
+          <button className="text-[10px] text-white py-2 px-3 font-600 leading-[19.2px] bg-[#303030] rounded-3xl">
             Apply to SIP 1.0
           </button>
         </div>
